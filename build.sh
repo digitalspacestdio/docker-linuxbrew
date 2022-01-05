e13ce57eafd88e73765d94497b1cf7ad201cfba2
scherepanov <sergey@cherepanov.org.ua>
2022-01-05 06:35:24 +0400

    fix build

--- build.sh
+++ build.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 cd $(dirname $0)
 BREW_VERSION=$(curl -L --silent https://api.github.com/repos/Homebrew/brew/releases | jq -r '.[].tag_name' | head -1)
-docker buildx build --platform linux/amd64,linux/arm64 --build-arg BREW_VERSION=${BREW_VERSION} -t digitalspacestudio/linuxbrew:${BREW_VERSION} .
-docker tag "digitalspacestudio/linuxbrew:${BREW_VERSION}" "digitalspacestudio/linuxbrew:latest"
-docker push "digitalspacestudio/linuxbrew:${BREW_VERSION}"
-docker push "digitalspacestudio/linuxbrew:latest"
+docker buildx build --push --platform linux/amd64,linux/arm64 --build-arg BREW_VERSION=${BREW_VERSION} -t "digitalspacestudio/linuxbrew:latest" -t digitalspacestudio/linuxbrew:${BREW_VERSION} .
